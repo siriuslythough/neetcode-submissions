2e9fa19ddf5947f2f2fa68cb76c3e1a5cb4ffdbf
siriuslythough <135163094+siriuslythough@users.noreply.github.com>
2026-06-24 03:34:57 +0530

    Update: linked-list-cycle-detection - submission-2

--- Data Structures & Algorithms/linked-list-cycle-detection/submission-2.cpp
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.cpp
@@ -11,38 +11,14 @@
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==nullptr) return list2;
-        if(list2==nullptr) return list1;
-        ListNode* t1 = list1;
-        ListNode* t2 = list2;
-
-        ListNode dummy(0); //dummy node with any value and nullptr as next (dummy IS A WHOLE NODE)
-        ListNode* a = &dummy; //a HOLDS THE EXACT ADDRESS OF dummy NOW
-                              //SO IF A CAUSES A CHANGE OF ADDRESS, THEN DUMMY ALSO SUFFERS A CHANGE OF ADDRESS 
-
-        while(t1!=nullptr && t2!=nullptr){
-            if(t1->val<t2->val){
-                a->next = t1; //HERE THE NEXT POINTER FROM a'S LOCATION, AND HENCE dummy's LOCATION ALSO
-                              //POINTS TO THE FIRST ELEMENT
-                t1=t1->next;
-            }
-            else if(t1->val==t2->val){
-                a->next = t1;
-                t1=t1->next; // only t1 is updated to move cause it has been seen by a, t2 wont be updated
-            }else{
-                a->next = t2;
-                t2=t2->next;
-
-            }
-            a = a->next; //THE FIRST TIME THIS IS DONE, a GOES TO A NEW LOCATION, WHERE dummy IS ALSO
-                              //POINTED TO. HENCE dummy POINTS TO FIRST NODE OF ANSWER LIST
+    bool hasCycle(ListNode* head) {
+        unordered_set<ListNode*> s;
+        ListNode* temp = head;
+        while(temp!=nullptr){
+            if(s.count(temp)) return true;
+            s.insert(temp);
+            temp = temp->next;
         }
-        if(t1!=nullptr){
-            a->next = t1;
-        }else{
-            a->next = t2;
-        }
-        return dummy.next; //a had been holding dummy at the start, and dummy pointed 
+        return false;
     }
 };
