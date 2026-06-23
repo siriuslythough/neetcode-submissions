725ef78e359b1a737daf32f64296c4e2cc7c1893
siriuslythough <135163094+siriuslythough@users.noreply.github.com>
2026-06-24 03:35:39 +0530

    Add: merge-k-sorted-linked-lists - submission-6

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-6.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists){
+        //the minheap approach: store a whole column of all lists in the minheap. store a pair of value and address.
+        //the minheap is the priority queue with {vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>} passed as arguments
+        //and when the minimum is found and pointed to, remove that element from pririty queue and add the next node value and address
+        priority_queue<pair<int, ListNode*>, 
+                       vector<pair<int, ListNode*>>, 
+                       greater<pair<int, ListNode*>>
+                      > pq;
+        ListNode dummy(0);
+        ListNode* temp = &dummy;
+        for(int i = 0; i<lists.size(); i++){
+            if (lists[i]) pq.push({lists[i]->val, lists[i]}); //store all heads
+        }
+        while(!pq.empty()){
+            pair<int, ListNode*> pr = pq.top(); //store the pair from minheap into a pair 
+            pq.pop(); //remove that from minheap
+            if(pr.second->next) pq.push({pr.second->next->val, pr.second->next}); //if not empty, add the next node from the list
+                                                                                  //where we had taken the node we just added
+            temp->next = pr.second; //add the pair address to dummy 
+            temp = temp->next; //move to that new node
+        }
+        return dummy.next;
+    }
+};
