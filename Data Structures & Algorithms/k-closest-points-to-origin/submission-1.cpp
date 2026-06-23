bc5dcc3a1a87844637a17d2b33957ed8b7436fe1
siriuslythough <135163094+siriuslythough@users.noreply.github.com>
2026-06-24 03:37:19 +0530

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cpp
@@ -0,0 +1,25 @@
+// why priority queue:
+// insertion & removal time: O(logn)
+// retrieval time: O(1)
+// size time: O(1)
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,vector<int>>> pq;
+        vector<vector<int>> ans;
+        for(int i = 0; i<points.size(); i++){
+            pq.push({dist(points[i]), points[i]});
+            if(pq.size()>k) pq.pop(); // the correct way to maintain size is to pop as soon as the size goes above k     
+        }
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+private:
+    int dist(vector<int> coord){
+        return (coord[0]*coord[0]) + (coord[1]*coord[1]); // dont use sqrt, just use squared euclidean distance
+    }
+};
