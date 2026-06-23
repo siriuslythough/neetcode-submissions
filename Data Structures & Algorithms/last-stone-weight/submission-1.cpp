347a407566797920d33a352704b649765584b806
siriuslythough <135163094+siriuslythough@users.noreply.github.com>
2026-06-24 03:37:13 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,23 @@
+// why priority queue:
+// insertion & removal time: O(logn)
+// retrieval time: O(1)
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i = 0; i<stones.size(); i++){
+            pq.push(stones[i]);
+        }
+        int h1, h2;
+        while(pq.size()>1){
+            h1 = pq.top();
+            pq.pop();
+            h2 = pq.top();
+            pq.pop();
+            if (h1!=h2) pq.push(h1-h2);
+        }
+        if(pq.size()) return pq.top();
+        return 0;
+    }
+};
