aaafceb064d72c817658cde592ee3b7a3b711384
siriuslythough <135163094+siriuslythough@users.noreply.github.com>
2026-07-28 23:33:10 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<vector<int>> adj(n+1);
+        for(const auto& vec : edges){
+            vector<bool> vis(n+1, false);
+            if (path_exists(vec[0], vec[1], vis, adj)) return {vec[0],vec[1]};
+            adj[vec[0]].push_back(vec[1]);
+            adj[vec[1]].push_back(vec[0]);
+        }
+        return {};
+    }
+private:
+    bool path_exists(int node, int target, vector<bool>& vis, vector<vector<int>>& adj){
+        if(node == target) return true;
+        vis[node] = true;
+        for(int nbd : adj[node]){
+            if(!vis[nbd]){
+                if(path_exists(nbd, target, vis, adj)) return true;
+            }
+        }
+        return false;
+    }
+};
