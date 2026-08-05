e89e13b753491529b639a5a7dd5f20b33f8492c5
siriuslythough <135163094+siriuslythough@users.noreply.github.com>
2026-08-05 17:15:19 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    int f(int ind, int buy, vector<int> &prices, vector<vector<int>>& dp){
+        if(ind >= prices.size()) return 0;
+        if(dp[ind][buy] != -1) return dp[ind][buy];
+        if(buy == 1){
+            return dp[ind][buy] = max(-prices[ind] + f(ind + 1, 0, prices, dp), 0 + f(ind+1, 1, prices, dp));
+        }
+        return dp[ind][buy] = max(prices[ind] + f(ind+2, 1, prices, dp), 0 + f(ind+1, 0, prices, dp));
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        // Initialize an N x 2 memoization table with -1
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return f(0, 1, prices, dp);
+    }
+};
