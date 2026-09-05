57e52a67f9ffac551a322e939268758d5a68150e
siriuslythough <135163094+siriuslythough@users.noreply.github.com>
2026-09-05 17:20:37 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        // "using the array as its own hash map"
+        int n = nums.size();
+        for(int i = 0; i<n; i++){
+            if(nums[i]<0) nums[i] = 0;
+        }
+        // use the sign of each element as a flag. if ith entry is negative means i+1 "value" exists in the array
+        for(int i = 0; i<n; i++){
+            int val = abs(nums[i]);
+            if(val>=1 && val<=n){
+                if(nums[val-1]>0) nums[val-1]*=-1;
+                else if (nums[val-1]==0) nums[val-1]=-1*(n+1);
+            }
+        }
+        for(int i = 1; i<=n; i++){
+            if(nums[i-1]>=0) return i;
+        }
+        return n+1;
+    }
+};
