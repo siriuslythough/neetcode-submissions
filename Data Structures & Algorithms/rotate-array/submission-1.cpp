f850c81e5ac7b951b22ee47b58f6f60c4bf3475b
siriuslythough <135163094+siriuslythough@users.noreply.github.com>
2026-09-05 18:33:10 +0530

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        // have an extra array, then copy it into nums
+        int n = nums.size();
+        vector<int> tmp(n);
+        for(int i = 0; i<n; i++) tmp[(i+k)%n] = nums[i];
+        for(int i = 0; i<n; i++) nums[i] = tmp[i];
+    }
+};
